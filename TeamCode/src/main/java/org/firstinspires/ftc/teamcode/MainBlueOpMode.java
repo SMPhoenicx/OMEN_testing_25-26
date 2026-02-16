602fd1505ea12719d51eded0eba5ae56ae427edf
Suman <sumanmuppavarapu@gmail.com>
2026-02-16 13:10:07 -0600

    post-metal-cf goal change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainBlueOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainBlueOpMode.java
@@ -169,7 +169,26 @@ public class MainBlueOpMode extends LinearOpMode
     private double flyTargetTicksPerSec = 0.0;
 
     private static final double[] CAM_RANGE_SAMPLES =   {25, 31.8, 37, 39.2, 44.2,  52.6, 53.1, 56.9, 61.5, 65.6, 70.3, 73.4, 77.5, 84.3, 91.8, 100.4, 110.0, 118.4};
-    private static final double[] ODOM_RANGE_SAMPLES =  {45.2, 50.2, 55.3, 60.9, 66.5, 72.2, 76.7, 81.1, 86.3, 90.9, 96.2, 99.7, 104.3, 109.9, 118.1, 128.5, 139.6, 148.7};
+    private static final double[] ODOM_RANGE_SAMPLES = {
+            44.7,  // was 45.2
+            49.7,  // was 50.2
+            54.7,  // was 55.3
+            60.3,  // was 60.9
+            65.8,  // was 66.5
+            71.4,  // was 72.2
+            75.9,  // was 76.7
+            80.3,  // was 81.1
+            85.4,  // was 86.3
+            90.0,  // was 90.9
+            95.2,  // was 96.2
+            98.7,  // was 99.7
+            103.2, // was 104.3
+            108.8, // was 109.9
+            116.9, // was 118.1
+            127.2, // was 128.5
+            138.2, // was 139.6
+            147.2  // was 148.7
+    };
     private static final double[] FLY_SPEEDS =          {1004, 1016, 1041, 1071, 1115, 1132, 1143, 1151, 1212, 1233, 1241, 1249, 1253, 1256, 1273, 1358, 1387, 1421};
     private static final double[] AIR_TIME =   {2.69, 2.79, 2.79, 2.79, 2.79, 2.79, 2.79, 2.89, 2.89, 2.89, 2.89, 2.89, 2.89, 3, 3.23, 3.5, 3.79, 4.27};  //seconds divide all by 4
     private static final double[] HOOD_ANGLES = GlobalOffsets.globalHoodAngles;
@@ -197,8 +216,8 @@ public class MainBlueOpMode extends LinearOpMode
     //endregion
 
     //region VARIANT VARS (Alliance Specific)
-    private static final double goalX = 0;
-    private static final double goalY = 144;
+    private static final double goalX = 1.5;
+    private static final double goalY = 142.5;
     private static final int DESIRED_TAG_ID = 20; //blue=20, red=24
     private static final Pose LOCALIZE_POSE = new Pose(144-9.125, 8.625, Math.toRadians(358.3));
     private Pose endgamePose = new Pose(40, 33, Math.toRadians(90));
